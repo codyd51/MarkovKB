2540c0c8ee7b3eaf46b5edff06b39ca3eb956bfc
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-19 15:41:53 -0700

    Initial Commit

---
+++ MarkovKB/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  MarkovKB
+//
+//  Created by Phillip Tennen on 6/19/16.
+//  Copyright © 2016 Phillip Tennen. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ MarkovKB/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  MarkovKB
+//
+//  Created by Phillip Tennen on 6/19/16.
+//  Copyright © 2016 Phillip Tennen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
