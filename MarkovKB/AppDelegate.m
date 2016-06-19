244f85bb009dd70b4f4e38a788a9bf70d5c595aa
Phillip Tennen <phillip.ennen@gmail.com>
2016-06-19 16:09:39 -0700

    Initial commit

--- MarkovKB/AppDelegate.m
+++ MarkovKB/AppDelegate.m
@@ -15,12 +15,32 @@
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+-(void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    /* create the NSTextView and add it to the window */
+    NSTextView* inputView = [[NSTextView alloc] initWithFrame:self.window.contentView.frame];
+    inputView.delegate = self;
+    [self.window setContentView:inputView];
+    [self.window makeKeyAndOrderFront:nil];
+    [self.window makeFirstResponder:inputView];
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
+-(void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+-(void)textDidChange:(NSNotification *)notification {
+    NSTextView* inputView = notification.object;
+    NSString* text = inputView.textStorage.string;
+    
+    //quit early if we have no text to work with
+    if (text.length < 1) return;
+    
+    //if the last character the user entered was a space or newline,
+    //update our predictions
+    unichar last = [text characterAtIndex:[text length] - 1];
+    if ([[NSCharacterSet whitespaceAndNewlineCharacterSet] characterIsMember:last]) {
+        
+    }
 }
 
 @end
